f15e950f30601a138c72de3d60c555670efbf103
Nati Pinyan <Natipinyan@gmail.com>
2025-03-20 00:56:42 +0200

    fix db file

--- sql/dbPro.sql
+++ sql/dbPro.sql
@@ -1,32 +1,29 @@
 CREATE TABLE IF NOT EXISTS `Sensors` (
-                                         `id` int AUTO_INCREMENT NOT NULL UNIQUE,
-                                         `name` varchar(255) NOT NULL,
-                                         `isRunning` int NOT NULL,
-                                         PRIMARY KEY (`id`)
+                                    `id` int AUTO_INCREMENT NOT NULL UNIQUE,
+                                    `name` varchar(255) NOT NULL,
+                                    `isRunning` int NOT NULL,
+                                     PRIMARY KEY (`id`)
     );
 
-CREATE TABLE IF NOT EXISTS `Trees` (
-                                       `id` int AUTO_INCREMENT NOT NULL UNIQUE,
-                                       `id_plants` int NOT NULL,
-                                       `max_temp` int NOT NULL,
-                                       `max_soil_moisture` int NOT NULL,
-                                       `date` date NOT NULL,
-                                       PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `Threes` (
+                                    `id` int AUTO_INCREMENT NOT NULL UNIQUE,
+                                    `id_plants` int NOT NULL,
+                                    `date` date NOT NULL,
+                                     PRIMARY KEY (`id`)
     );
 
 CREATE TABLE IF NOT EXISTS `Plants` (
-                                        `id` int AUTO_INCREMENT NOT NULL UNIQUE,
-                                        `name` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
+                                    `id` int AUTO_INCREMENT NOT NULL UNIQUE,
+                                    `name` varchar(255) NOT NULL,
+                                     PRIMARY KEY (`id`)
     );
 
 CREATE TABLE IF NOT EXISTS `DataSensors` (
-                                             `id` int AUTO_INCREMENT NOT NULL UNIQUE,
-                                             `id_trees` int NOT NULL,
-                                             `id_sensors` int NOT NULL,
-                                             `avg` double NOT NULL,
-                                             `date` date NOT NULL,
-                                             PRIMARY KEY (`id`)
+                                    `id` int AUTO_INCREMENT NOT NULL UNIQUE,
+                                    `id_trees` int NOT NULL,
+                                    `totalirrigation` double NOT NULL,
+                                    `date` date NOT NULL,
+                                    PRIMARY KEY (`id`)
     );
 
 ALTER TABLE `Trees` ADD CONSTRAINT `Trees_fk1` FOREIGN KEY (`id_plants`) REFERENCES `Plants`(`id`);

878ea78735a76a2213962eba1013cfce371830a0
Nati Pinyan <Natipinyan@gmail.com>
2025-03-20 00:35:13 +0200

    add end point to delete and read data

--- server/models/treeMode.js
+++ server/models/treeMode.js
@@ -3,28 +3,61 @@ class Tree{
         this.DB = db;
     }
 
-    async getAllTree(){
-        // let [sql,t]= await this.DB.execute(`SELECT * FROM threes,plants WHERE id_plants = id`);
+    async readAllTree() {
+        try {
+            const [sql, t] = await this.DB.execute(`SELECT * FROM threes`);
+
+            return sql;
+        } catch (error) {
+            console.log("Error fetching trees:", error);
+            throw error;
+        }
     }
 
     async createTree(nameTree){
         try {
+            console.log(nameTree);
             const date = new Date();
-            const formattedDate = date.toISOString().split('T')[0];
-            let [sql,t]= await this.DB.execute(`SELECT * FROM plants where name = ?`,[nameTree]);
-            if(sql.length > 0){
-                await this.DB.execute(`INSERT INTO threes(id_plants, date) VALUE(?,?);`,[sql[0].id, formattedDate]);
-            }else{
-                sql = await this.DB.execute(`INSERT INTO plants(name) VALUE(?);`,[nameTree]);
-                await this.DB.execute(`INSERT INTO threes(id_plants, date) VALUE(?,?);`,[sql.insertId, formattedDate]);
-                console.log(sql);
-            }
+             const formattedDate = date.toISOString().split('T')[0];
+             let [sql,t]= await this.DB.execute(`SELECT * FROM plants where name = ?`,[nameTree]);
+             if(sql.length > 0){
+                 await this.DB.execute(`INSERT INTO threes(id_plants, date) VALUE(?,?);`,[sql[0].id, formattedDate]);
+             }else{
+                 sql = await this.DB.execute(`INSERT INTO plants(name) VALUE(?);`,[nameTree]);
+                 console.log(sql[0].insertId);
+                 console.log(formattedDate);
+                 let answer =  await this.DB.execute(`INSERT INTO threes(id_plants, date) VALUE(?,?);`,[sql[0].insertId, formattedDate]);
+             }
+
         } catch (error) {
             console.log(error);
         }
-
-
     }
+
+    async deleteTreeById(id) {
+
+        const [sql, t] = await this.DB.execute(`DELETE FROM threes WHERE id='${id}'`);
+
+        console.log(sql);
+        db_pool.query(sql, function(err, rows, fields){
+            if(err){
+                res.status(500).json({message: err})
+            }else{
+                res.status(200).json({message: "OK"});
+            }
+        });
+    }
+
+    async deleteTreeById(id) {
+        try {
+            const sql = await this.DB.execute(`DELETE FROM threes WHERE id='${id}'`);
+        } catch (error) {
+            console.log("Error deleting tree:", error);
+            throw error;
+        }
+    }
+
+
 
 }
 
--- server/routers/treeRout.js
+++ server/routers/treeRout.js
@@ -16,7 +16,29 @@ router.post("/add", (req, res) => {
 
 });
 
+router.get("/get", async (req, res) => {
+    try {
+        const trees = await tree.readAllTree();
 
+        res.status(200).json({ message: "Trees fetched successfully", data: trees });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error fetching trees", error: error.message });
+    }
+});
+
+
+router.delete("/Delete/:row_id",(req, res) => {
+    try {
+        let id = req.params.row_id;
+        const trees =  tree.deleteTreeById(id);
+
+        res.status(200).json({ message: "Trees deleting successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error deleting tree", error: error.message });
+    }
+});
 
 
 module.exports = router;
